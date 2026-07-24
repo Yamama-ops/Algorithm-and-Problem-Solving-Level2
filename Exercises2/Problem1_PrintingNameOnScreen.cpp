4fefed2cb2fdcf172effd62b713b532fbd349a09
Yamama-ops <yamamaa084@gmail.com>
2026-07-24 11:12:41 +0300

    Create Problem1_PrintingNameOnScreen.cpp

---
+++ Exercises2/Problem1_PrintingNameOnScreen.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void PrintNameOnScreen(string Name)
+{
+	cout << "Your name is: " << Name << endl;
+}
+int main()
+{
+	PrintNameOnScreen("Yamama");
+	return 0;
+}
